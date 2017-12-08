aef8d336c55b35fe485172173a131c8b11744219
Carol Greenberg <look@theuxincubator.com>
2017-12-07 16:24:16 -0800

    test

---
+++ CareGiverApp/ClientsTableView.swift
@@ -0,0 +1,14 @@
+//
+//  ClientsTableView.swift
+//  CareGiverApp
+//
+//  Created by Carol Greenberg on 12/6/17.
+//  Copyright © 2017 Carol Greenberg. All rights reserved.
+//
+
+import UIKit
+
+class clientsTableView: UITableView {
+    
+   // var clients:[Client] = []
+}
