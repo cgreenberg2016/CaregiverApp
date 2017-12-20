9f6c53a0f340fca6395976067d72bdc1cb63e3b8
Carol Greenberg <look@theuxincubator.com>
2017-12-19 19:11:23 -0800

    fixed error that crashed Xcode

--- CareGiverApp/view controllers/ContactsTableViewController.swift
+++ CareGiverApp/view controllers/ContactsTableViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 import CoreData
 
 class ContactsTableViewController: UITableViewController {
-
-   
+    
+    
     @IBOutlet var TableContainer: UITableView!
     
     var contacts:[ContactsEntity] = []
@@ -20,17 +20,17 @@ class ContactsTableViewController: UITableViewController {
         TableContainer?.delegate = self
         TableContainer?.dataSource = self as UITableViewDataSource
         print("viewDidLoad happened")
-       
         
-      /*  let moveButton = UIBarButtonItem(barButtonSystemItem: .edit,  target: self, action: #selector(ClientsTableViewController.toggleEdit))
-        navigationItem.leftBarButtonItem = moveButton*/
         
-
+        /*  let moveButton = UIBarButtonItem(barButtonSystemItem: .edit,  target: self, action: #selector(ClientsTableViewController.toggleEdit))
+         navigationItem.leftBarButtonItem = moveButton*/
+        
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-         self.navigationItem.leftBarButtonItem = self.editButtonItem
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
     }
     override func viewWillAppear(_ animated: Bool) {
         //get data from core data
@@ -62,35 +62,30 @@ class ContactsTableViewController: UITableViewController {
     }
     
     // manually show segue
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let contact = self.contacts[indexPath.row]
-        if
-            let destination = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "ContactsViewController") as? ContactsViewController {
-            destination.contact = contact
-            self.navigationController?.pushViewController(destination, animated: true)
-        }
-    }
+    /*   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+     let contact = self.contacts[indexPath.row]
+     if
+     let destination = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "ContactsViewController") as? ContactsViewController {
+     destination.contact = contact
+     self.navigationController?.pushViewController(destination, animated: true)
+     }
+     } */
     
-   
+    
     
     // delete items in list
-  /*  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistantContainer2.viewContext
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistantContainer.viewContext
         
         if editingStyle == .delete {
-            let task = contacts[indexPath.row]
-            context.delete(task)
+            let contact = contacts[indexPath.row]
+            context.delete(contact)
             (UIApplication.shared.delegate as! AppDelegate).saveContext()
             
-            do {
-                contacts = try context.fetch(ContactsEntity.fetchRequest()) as! [Contact]
-            }
-            catch {
-                print ("Fetching failed")
-            }
+            getData()
+            tableView.reloadData()
         }
-        tableView.reloadData()
-    } */
+    }
     
     @objc func toggleEdit() {
         tableView.setEditing(!tableView.isEditing, animated: true)
@@ -105,77 +100,77 @@ class ContactsTableViewController: UITableViewController {
     
     @objc func addContact() {
         let context =  (UIApplication.shared.delegate as! AppDelegate).persistantContainer.viewContext
-    // the add button should go to the next screen. when the user inputs all the information, there should be a save button that goes back to the contact list screen that shows the contact
+        // the add button should go to the next screen. when the user inputs all the information, there should be a save button that goes back to the contact list screen that shows the contact
         
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         
-//        let newContact = Contact(name: "Enter New Contact", phone: "Enter New Phone Number")
-//        self.contacts.append(newContact)
-//        let newIndexPath = IndexPath(row: self.contacts.count - 1, section: 0)
-//        self.tableView.insertRows(at: [newIndexPath], with: .automatic)
+        //        let newContact = Contact(name: "Enter New Contact", phone: "Enter New Phone Number")
+        //        self.contacts.append(newContact)
+        //        let newIndexPath = IndexPath(row: self.contacts.count - 1, section: 0)
+        //        self.tableView.insertRows(at: [newIndexPath], with: .automatic)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
-  
+    
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-
     
-
+    
+    
     
     // Override to support editing the table view.
-  //  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-   //     if editingStyle == .delete {
-            // Delete the row from the data source
+    //  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    //     if editingStyle == .delete {
+    // Delete the row from the data source
     //        tableView.deleteRows(at: [indexPath], with: .fade)
-   //     } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    //     } else if editingStyle == .insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     //    }
-  //  }
+    //  }
     
-
+    
     
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+        
     }
     
-
-   
+    
+    
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
     
-
+    
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
- /*   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "DisplayContact" {
             let indexPath = self.tableView.indexPath(for: sender as! UITableViewCell)!
             let contact = self.contacts[indexPath.row]
             let destination = segue.destination as! ContactsViewController
             destination.contact = contact
         }
-   
-            } */
-        }
+        
+    }
+}
         
 
     
--- CareGiverApp/view controllers/ContactsViewController.swift
+++ CareGiverApp/view controllers/ContactsViewController.swift
@@ -1,128 +1,129 @@
-                    //
-                    //  ContactsViewController.swift
-                    //  CareGiverApp
-                    //
-                    //  Created by Carol Greenberg on 11/21/17.
-                    //  Copyright © 2017 Carol Greenberg. All rights reserved.
-                    //
-                    
+//
+//  ContactsViewController.swift
+//  CareGiverApp
+//
+//  Created by Carol Greenberg on 11/21/17.
+//  Copyright © 2017 Carol Greenberg. All rights reserved.
+//
+
 import UIKit
 import CoreData
-                    
+
 class ContactsViewController: UIViewController {
-                        
-@IBOutlet weak var ScrollView: UIScrollView!
-                        
-@IBOutlet weak var AddressView: UIView!
-                        
-@IBOutlet weak var nameLabel: UITextField!
-@IBOutlet weak var phoneLabel: UITextField!
-@IBOutlet weak var address1Label: UITextField!
-@IBOutlet weak var address2Label: UITextField!
-@IBOutlet weak var cityLabel: UITextField!
-@IBOutlet weak var stateLabel: UITextField!
-@IBOutlet weak var zipcodeLabel: UITextField!
-@IBOutlet weak var emailLabel: UITextField!
-var contact: ContactsEntity?
-                        
+    
+    @IBOutlet weak var ScrollView: UIScrollView!
+    
+    @IBOutlet weak var AddressView: UIView!
+    
+    @IBOutlet weak var nameLabel: UITextField!
+    @IBOutlet weak var phoneLabel: UITextField!
+    @IBOutlet weak var address1Label: UITextField!
+    @IBOutlet weak var address2Label: UITextField!
+    @IBOutlet weak var cityLabel: UITextField!
+    @IBOutlet weak var stateLabel: UITextField!
+    @IBOutlet weak var zipcodeLabel: UITextField!
+    @IBOutlet weak var emailLabel: UITextField!
+    var contact: ContactsEntity?
+    
     @IBAction func DoneButton(_ sender: UIBarButtonItem) {
         let context =  (UIApplication.shared.delegate as! AppDelegate).persistantContainer.viewContext
+        if let contact = contact {
+            contact.contactName = nameLabel.text
+            contact.contactPhone = phoneLabel.text
+            contact.contactAddress1 = address1Label.text
+            contact.contactAddress2 = address2Label.text
+            contact.contactCity = cityLabel.text
+            contact.contactState = stateLabel.text
+            contact.contactZip = zipcodeLabel.text
+            contact.contactEmail = emailLabel.text
+        }
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         navigationController!.popViewController(animated: true)
-       let contact = ContactsEntity(context: context)
-        contact.contactName = nameLabel.text
-        contact.contactPhone = phoneLabel.text
-        contact.contactAddress1 = address1Label.text
-        contact.contactAddress2 = address2Label.text
-        contact.contactCity = cityLabel.text
-        contact.contactState = stateLabel.text
-        contact.contactZip = zipcodeLabel.text
-        contact.contactEmail = emailLabel.text
-      //  (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
     }
     
     override func viewDidLoad() {
-    super.viewDidLoad()
-                            
-    let notificationCenter = NotificationCenter.default
-    notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: Notification.Name.UIKeyboardWillHide, object: nil)
-    notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: Notification.Name.UIKeyboardWillChangeFrame, object: nil)
-    if contact == nil {
-        let context =  (UIApplication.shared.delegate as! AppDelegate).persistantContainer.viewContext
-        contact = ContactsEntity(context: context)
-
+        super.viewDidLoad()
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: Notification.Name.UIKeyboardWillHide, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: Notification.Name.UIKeyboardWillChangeFrame, object: nil)
+        if contact == nil {
+            let context =  (UIApplication.shared.delegate as! AppDelegate).persistantContainer.viewContext
+            contact = ContactsEntity(context: context)
+            
+            
+            
+        }
         
         
+        
+        self.nameLabel.delegate = self as? UITextFieldDelegate
+        self.phoneLabel.delegate = self as? UITextFieldDelegate
+        self.address1Label.delegate = self as? UITextFieldDelegate
+        self.address2Label.delegate = self as? UITextFieldDelegate
+        self.cityLabel.delegate = self as? UITextFieldDelegate
+        self.stateLabel.delegate = self as? UITextFieldDelegate
+        self.zipcodeLabel.delegate = self as? UITextFieldDelegate
+        self.emailLabel.delegate = self as? UITextFieldDelegate
+        
+        if let contact = self.contact {
+            if let name = contact.contactName {
+                self.nameLabel.text = name
+            }
+            if let phone = contact.contactPhone {
+                self.phoneLabel.text = phone
+            }
+            if let address1 = contact.contactAddress1 {
+                self.address1Label.text = address1
+            }
+            if let address2 = contact.contactAddress2 {
+                self.address2Label.text = address2
+            }
+            if let city = contact.contactCity {
+                self.cityLabel.text = city
+            }
+            if let state = contact.contactState {
+                self.stateLabel.text = state
+            }
+            if let zipcode = contact.contactZip {
+                self.zipcodeLabel.text = zipcode
+            }
+            if let email = contact.contactEmail {
+                self.emailLabel.text = email
+            }
+            
+            
+        }
     }
-                            
-                           
-                            
-    self.nameLabel.delegate = self as? UITextFieldDelegate
-    self.phoneLabel.delegate = self as? UITextFieldDelegate
-    self.address1Label.delegate = self as? UITextFieldDelegate
-    self.address2Label.delegate = self as? UITextFieldDelegate
-    self.cityLabel.delegate = self as? UITextFieldDelegate
-    self.stateLabel.delegate = self as? UITextFieldDelegate
-    self.zipcodeLabel.delegate = self as? UITextFieldDelegate
-    self.emailLabel.delegate = self as? UITextFieldDelegate
-                            
-    if let contact = self.contact {
-        if let name = contact.contactName {
-            self.nameLabel.text = name
-    }
-    if let phone = contact.contactPhone {
-        self.phoneLabel.text = phone
-    }
-    if let address1 = contact.contactAddress1 {
-        self.address1Label.text = address1
-    }
-    if let address2 = contact.contactAddress2 {
-        self.address2Label.text = address2
-    }
-    if let city = contact.contactCity {
-        self.cityLabel.text = city
-    }
-    if let state = contact.contactState {
-        self.stateLabel.text = state
-    }
-    if let zipcode = contact.contactZip {
-        self.zipcodeLabel.text = zipcode
-    }
-    if let email = contact.contactEmail {
-        self.emailLabel.text = email
-    }
-                                
-                                
-    }
-}
-                        
-                        
-                        
-override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-                        
+    
     @objc func adjustForKeyboard(notification: Notification) {
         let userInfo = notification.userInfo!
-                            
+        
         let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
         let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
-                            
+        
         if notification.name == Notification.Name.UIKeyboardWillHide {
-                                ScrollView.contentInset = UIEdgeInsets.zero
-            } else {
-                        ScrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
-                }
-                            
-                    ScrollView.scrollIndicatorInsets = ScrollView.contentInset
-                            
-                          //  let selectedRange = ScrollView.selectedRange
-                          //  ScrollView.scrollRangeToVisible(selectedRange)
+            ScrollView.contentInset = UIEdgeInsets.zero
+        } else {
+            ScrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+        
+        ScrollView.scrollIndicatorInsets = ScrollView.contentInset
+        
+        //  let selectedRange = ScrollView.selectedRange
+        //  ScrollView.scrollRangeToVisible(selectedRange)
     }
-                        
+    
 }
-                    
-                    
-                    
-                    
+
+
+
+
